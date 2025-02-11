ebcb00bbf6874e43a162c941f0cfcc5120a363fa
vaspurak avdalian <vaspurakavdalyani@gmail.com>
2025-02-11 22:03:48 +0100

    fetch user data from API on component mount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:3000/users");
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        throw new Error("Oops, something went wrong", error);
+      }
+    }
+    fetchData();
+  }, []);
   return <div className="App"></div>;
 }
 
