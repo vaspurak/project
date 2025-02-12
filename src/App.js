d93f3deb0bebb8d080c3910a53032022355d392e
vaspurak avdalian <vaspurakavdalyani@gmail.com>
2025-02-12 13:35:08 +0100

    refactor App component and add TodoList for user data display; configure json-server for local API

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import { useEffect, useState } from "react";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await fetch("http://localhost:3000/users");
-        const data = await res.json();
-        setUsers(data);
-        console.log(data);
-      } catch (error) {
-        throw new Error("Oops, something went wrong", error);
-      }
-    }
-    fetchData();
-  }, []);
   return (
-    <div className="App">
-      <ul className="container">
-        {users.map((user) => (
-          <li key={user.id}>
-            <h1>{user.name}</h1>
-            <h2>{user.username}</h2>
-            <span>{user.email}</span>
-            <ul>
-              {user.posts.map((post) => (
-                <li key={post.post_id}>
-                  <h3>{post.title}</h3>
-                  <p>{post.content}</p>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+function TodoList() {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/users");
+      if (!res.ok) {
+        throw new Error("Ошибка загрузки пользователей");
+      }
+      const data = await res.json();
+      setUsers(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <ul className="container">
+        {users.map((user) => (
+          <li key={user.id}>
+            <h1>{user.name}</h1>
+            <h2>{user.username}</h2>
+            <span>{user.email}</span>
+            <ul>
+              {user.posts && user.posts.length > 0 ? (
+                user.posts.map((post) => (
+                  <li key={post.post_id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.content}</p>
+                  </li>
+                ))
+              ) : (
+                <p>No post available</p>
+              )}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
